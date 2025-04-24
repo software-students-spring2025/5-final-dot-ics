035b6e2a3d7060da7574b5ceb9ec2089bf3a3708
a-wyndham1 <apollo_wyndham@yahoo.com>
2025-04-24 13:06:29 -0400

    use actual db

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -16,14 +16,6 @@ TEST_MONGO_DBNAME = "test_db"
 def flask_app():
     app = create_app()
     app.config.update({"TESTING": True, "WTF_CSRF_ENABLED": False})
-
-    # Wait a moment for MongoDB connection to be established
-    time.sleep(1)
-
-    mongo_client = app.extensions.get("pymongo")
-    if mongo_client:
-        db = mongo_client[TEST_MONGO_DBNAME]
-
     return app
 
 @pytest.fixture
@@ -43,8 +35,6 @@ def mongodb():
     assert client.admin.command("ping")["ok"] != 0.0  # Check that the connection is okay.
     return client
 
-
-
 def test_create_user(client, mongodb):
     """
     test_create_user tests creating a new user and logging in.
@@ -56,7 +46,7 @@ def test_create_user(client, mongodb):
     
     assert response.status_code == 200
     
-    user = mongodb.test_db.users.find_one({"username": "testuser"})
+    user = mongodb["dot-ics"].users.find_one({"username": "testuser"})
 
     assert user is not None
     assert user["username"] ==  "testuser"
@@ -66,7 +56,7 @@ def test_login(client, mongodb):
     test_login tests logging in with the created user.
     """
 
-    mongodb.test_db.users.insert_one({"username": "testuser", "password": "password"})
+    mongodb["dot-ics"].users.insert_one({"username": "testuser", "password": "password"})
 
     response = client.post('/login', data=dict(
         username='testuser',
@@ -82,7 +72,7 @@ def test_logout(client, mongodb):
     test_logout tests logging out the user.
     """
 
-    mongodb.test_db.users.insert_one({"username": "testuser", "password": "password"})
+    mongodb["dot-ics"].users.insert_one({"username": "testuser", "password": "password"})
 
     client.post('/login', data=dict(
         username='testuser',
@@ -91,8 +81,6 @@ def test_logout(client, mongodb):
 
     response = client.get('/logout', follow_redirects=True)
     
-    client.assertRedirects(response, '/')
-
     with client:
         response = client.get('/')
         assert b"testuser" not in response.data
@@ -102,9 +90,9 @@ def test_index_page(client, mongodb):
     test_index_page tests the index page when a user is logged in.
     """
 
-    user =  mongodb.test_db.users.insert_one({"username": "testuser", "password": "password"})
+    user =  mongodb["dot-ics"].users.insert_one({"username": "testuser", "password": "password"})
 
-    mongodb.test_db.events.insert_one({
+    mongodb["dot-ics"].events.insert_one({
         "user_id": user.inserted_id,
         "name": "Test Event",
         "start_time": "2025-04-21 10:00:00",
